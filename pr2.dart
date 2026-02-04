135dee35dd66cbd1753c6b4a1f7f8675affb28db
Khaled abdelhady <Khaledabdelhady796@gmail.com>
2026-02-04 17:11:36 +0200

    Update code content

---
+++ pr2.dart
@@ -0,0 +1,13 @@
+void main( )
+{
+  var name = "khaled elnaby" ;
+  var age = 19;
+  print (name) ; 
+  print (age) ;
+   print ("$name") ; // لو عايز اكتب المتغير بين "" لازم اكتب $
+  print ("$age") ;
+  print ("How Are You $name Age $age");
+
+
+
+}
