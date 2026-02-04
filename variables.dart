85b07260f3d0f9257d26b6840cd0e99f3dc36b72
Khaled abdelhady <Khaledabdelhady796@gmail.com>
2026-02-04 18:51:17 +0200

    first commit

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  const String name = "khaled" ;   // غير مسمح التعديل فيه
+  String fullName = "khaled elnaby" ; // مسموح التعديل فيه 
+  int age = 19 ; 
+  double h = 181.2 ; 
+  bool married = false ;
+  print (name) ;
+  print (fullName) ;
+  print (age ) ;
+  print ( h ) ;
+  print ( married ) ;
+  
+
+
+}
